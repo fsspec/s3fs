39534068b3afae74504c6656bb779ac436eebb0c
Alex Rogozhnikov <iamfullofspam@gmail.com>
2024-08-03 15:15:02 -0700

    use `fixed_upload_size` argument  to switch between old and new behavior

--- s3fs/core.py
+++ s3fs/core.py
@@ -296,6 +296,7 @@ class S3FileSystem(AsyncFileSystem):
         asynchronous=False,
         loop=None,
         max_concurrency=1,
+        fixed_upload_size: int | None = None,
         **kwargs,
     ):
         if key and username:
@@ -333,6 +334,9 @@ class S3FileSystem(AsyncFileSystem):
         self.cache_regions = cache_regions
         self._s3 = None
         self.session = session
+        if fixed_upload_size is not None and fixed_upload_size < self.blocksize:
+            raise ValueError(f"{fixed_upload_size=} is less than {self.blocksize=}")
+        self.fixed_upload_size = fixed_upload_size
         if max_concurrency < 1:
             raise ValueError("max_concurrency must be >= 1")
         self.max_concurrency = max_concurrency
@@ -2161,7 +2165,6 @@ class S3File(AbstractBufferedFile):
             raise ValueError("ACL not in %s", key_acls)
         self.mpu = None
         self.parts = None
-        self.awaits_upload = b""  # for multi-part uploads
         self.fill_cache = fill_cache
         self.s3_additional_kwargs = s3_additional_kwargs or {}
         self.req_kw = {"RequestPayer": "requester"} if requester_pays else {}
@@ -2330,17 +2333,11 @@ class S3File(AbstractBufferedFile):
             and not self.append_block
             and final
             and self.tell() < self.blocksize
-            and not self.awaits_upload
         ):
             # only happens when closing small file, use on-shot PUT
             pass
         else:
             self.buffer.seek(0)
-
-            # previously last small chunk was merged into previous one
-            # (and in some cases two chunks were equalized)
-            # However, R2 imposes additional restriction that all but last chunk
-            # are of same size, and last chunk can't be larger.
 
             def upload_part(part_data: bytes):
                 if len(part_data) == 0:
@@ -2362,17 +2359,18 @@ class S3File(AbstractBufferedFile):
                     part_header["ChecksumSHA256"] = out["ChecksumSHA256"]
                 self.parts.append(part_header)
 
-            while received := self.buffer.read(self.blocksize):
-                # send in chunks of same size
-                self.awaits_upload += received
-                while len(self.awaits_upload) >= self.blocksize:
-                    upload_part(self.awaits_upload[: self.blocksize])
-                    self.awaits_upload = self.awaits_upload[self.blocksize :]
+            def n_bytes_left() -> int:
+                return len(self.buffer.getbuffer()) - self.buffer.tell()
 
-            if final:
-                # send anything in the buffer
-                upload_part(self.awaits_upload)
-                self.awaits_upload = b""
+            if self.fs.fixed_upload_size is not None:
+                # all chunks have fixed size, exception: last one can be smaller
+                min_chunk = 1 if final else self.fs.fixed_upload_size
+                while n_bytes_left() >= self.fs.fixed_upload_size:
+                    upload_part(self.buffer.read(self.fixed_upload_size))
+            else:
+                min_chunk = 1 if final else self.blocksize
+                while n_bytes_left() >= min_chunk:
+                    upload_part(self.buffer.read(self.part_max))
 
         if self.autocommit and final:
             self.commit()
