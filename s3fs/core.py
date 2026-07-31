712843d50c3d44d3a6014b4694bbe60304b1de87
Julian Sikorski <belegdol+github@gmail.com>
2026-07-31 22:52:06 +0200

    Include ServerSideEncryption in_ info() output.
    This makes it possible to check whether ETag contains MD5 hash or not. Fixes #1037.

--- s3fs/core.py
+++ s3fs/core.py
@@ -1754,6 +1754,7 @@ class S3FileSystem(AsyncFileSystem):
                     "StorageClass": out.get("StorageClass", "STANDARD"),
                     "VersionId": out.get("VersionId"),
                     "ContentType": out.get("ContentType"),
+                    "ServerSideEncryption": out.get("ServerSideEncryption"),
                 }
             except FileNotFoundError:
                 pass
